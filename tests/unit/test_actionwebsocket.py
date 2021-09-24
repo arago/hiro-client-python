dd0cc67ca9d78e4876c9c86878f73382c596b486
Wolfgang Hübner <duke@arago.de>
2021-09-24 20:09:16 +0200

    Externalize test credentials

--- tests/unit/test_actionwebsocket.py
+++ tests/unit/test_actionwebsocket.py
@@ -1,35 +1,73 @@
-# from hiro_graph_client.actionwebsocket import AbstractActionWebSocketHandler
-# from hiro_graph_client.clientlib import PasswordAuthTokenApiHandler, AbstractTokenApiHandler, accept_all_certs
+import concurrent.futures
+import threading
+
+from hiro_graph_client.actionwebsocket import AbstractActionWebSocketHandler
+from hiro_graph_client.clientlib import PasswordAuthTokenApiHandler, AbstractTokenApiHandler, accept_all_certs
+from testconfig import CONFIG
 
 
-# class ActionWebSocket(AbstractActionWebSocketHandler):
-#
-#     def __init__(self, api_handler: AbstractTokenApiHandler):
-#         super().__init__(api_handler)
-#
-#     def on_submit_action(self, action_id: str, capability: str, parameters: dict):
-#         print(f"ID: {action_id}, Capability: {capability}, Parameters: {str(parameters)}")
-#
-#         self.send_action_result(action_id, "Everything went fine.")
-#         #     raise Exception("This is a test exception.")
-#
-#     def on_config_changed(self):
-#         pass
+def __init__(self, api_handler: AbstractTokenApiHandler):
+    """ Initialize properties """
+    super().__init__(api_handler)
+    self._executor = None
+
+
+class ActionWebSocket(AbstractActionWebSocketHandler):
+
+    def __init__(self, api_handler: AbstractTokenApiHandler):
+        """ Initialize properties """
+        super().__init__(api_handler)
+        self._executor = None
+
+    def start(self) -> None:
+        """ Initialize the executor """
+        super().start()
+        self._executor = concurrent.futures.ThreadPoolExecutor()
+
+    def stop(self, timeout: int = None) -> None:
+        """ Shut the executor down """
+        if self._executor:
+            self._executor.shutdown()
+        self._executor = None
+        super().stop(timeout)
+
+    def handle_submit_action(self, action_id: str, capability: str, parameters: dict):
+        """ Runs asynchronously in its own thread. """
+        print(f"ID: {action_id}, Capability: {capability}, Parameters: {str(parameters)}")
+        self.send_action_result(action_id, "Everything went fine.")
+
+    def on_submit_action(self, action_id: str, capability: str, parameters: dict):
+        """ Message *submitAction* has been received. Message is handled in thread executor. """
+        if not self._executor:
+            raise RuntimeError('ActionWebSocket has not been started.')
+        self._executor.submit(ActionWebSocket.handle_submit_action, self, action_id, capability, parameters)
+
+    def on_config_changed(self):
+        """ The configuration of the ActionHandler has changed """
+        pass
 
 
 class TestActionWebsocket:
-    # api_handler = PasswordAuthTokenApiHandler(
-    #     root_url="",
-    #     username='',
-    #     password='',
-    #     client_id='',
-    #     client_secret='',
-    #     secure_logging=False
-    # )
-    #
+    api_handler = PasswordAuthTokenApiHandler(
+        root_url=CONFIG.get('URL'),
+        username=CONFIG.get('USERNAME'),
+        password=CONFIG.get('PASSWORD'),
+        client_id=CONFIG.get('CLIENT_ID'),
+        client_secret=CONFIG.get('CLIENT_SECRET'),
+        secure_logging=False,
+        client_name="Test-client"
+    )
+
+    @staticmethod
+    def wait_for_keypress(ws: ActionWebSocket):
+        input("Press [Enter] to stop.\n")
+        ws.stop()
+
     def test_actions(self):
-        # accept_all_certs()
-        #
-        # with ActionWebSocket(api_handler=self.api_handler) as ws:
-        #     input("Press [Enter] to stop.\n")
-        pass
+        accept_all_certs()
+
+        with ActionWebSocket(api_handler=self.api_handler) as ws:
+            threading.Thread(daemon=True,
+                             target=lambda _ws: TestActionWebsocket.wait_for_keypress(_ws),
+                             args=(ws,)).start()
+            ws.run_forever()
--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,89 +1,76 @@
-# from typing import Any, Iterator
-#
-# from hiro_graph_client import PasswordAuthTokenApiHandler, AbstractTokenApiHandler, HiroGraph
-# from hiro_graph_client.batchclient import HiroResultCallback, HiroGraphBatch
-#
-#
-# class RunBatch(HiroResultCallback):
-#     hiro_batch_client: HiroGraphBatch
-#
-#     def __init__(self, api_handler: AbstractTokenApiHandler):
-#         self.hiro_batch_client = HiroGraphBatch(
-#             callback=self,
-#             api_handler=api_handler)
-#
-#     def result(self, data: Any, code: int) -> None:
-#         print('Data: ' + str(data))
-#         print('Code: ' + str(code))
-#
-#     def _run(self, commands: Iterator[dict]):
-#         self.hiro_batch_client.multi_command(commands)
-#
-#
+from typing import Any, Iterator
+
+from hiro_graph_client import PasswordAuthTokenApiHandler, AbstractTokenApiHandler, HiroGraph, SSLConfig
+from hiro_graph_client.batchclient import HiroResultCallback, HiroGraphBatch
+from testconfig import CONFIG
+
+
+class RunBatch(HiroResultCallback):
+    hiro_batch_client: HiroGraphBatch
+
+    def __init__(self, api_handler: AbstractTokenApiHandler):
+        self.hiro_batch_client = HiroGraphBatch(
+            callback=self,
+            api_handler=api_handler)
+
+    def result(self, data: Any, code: int) -> None:
+        print('Data: ' + str(data))
+        print('Code: ' + str(code))
+
+    def run(self, commands: Iterator[dict]):
+        self.hiro_batch_client.multi_command(commands)
+
+
 class TestClient:
-    # hiro_api_handler = PasswordAuthTokenApiHandler(
-    #     root_url='',
-    #     username='',
-    #     password='',
-    #     client_id='',
-    #     client_secret='',
-    #     secure_logging=False,
-    #     custom_endpoints={
-    #         'graph': '/api/graph/7.2'
-    #     },
-    #     headers={
-    #         'user-agent': 'TestClient'
-    #     }
-    # )
-    #
+    hiro_api_handler = PasswordAuthTokenApiHandler(
+        root_url=CONFIG.get('URL'),
+        username=CONFIG.get('USERNAME'),
+        password=CONFIG.get('PASSWORD'),
+        client_id=CONFIG.get('CLIENT_ID'),
+        client_secret=CONFIG.get('CLIENT_SECRET'),
+        secure_logging=False,
+        ssl_config=SSLConfig(verify=False)
+    )
+
     def test_simple_query(self):
-        pass
-        # hiro_client: HiroGraph = HiroGraph(api_handler=self.hiro_api_handler)
-        #
-        # query_result: dict = hiro_client.query('ogit\\/_type:"ogit/MARS/Machine"', limit=1, meta=True)
-        #
-        # print(query_result)
-        #
-        # assert isinstance(query_result, dict)
+        hiro_client: HiroGraph = HiroGraph(api_handler=self.hiro_api_handler)
 
     def test_batch_command(self):
-        pass
-        # hiro_batch_client: HiroGraphBatch = HiroGraphBatch(api_handler=self.hiro_api_handler)
-        #
-        # commands: list = [
-        #     {
-        #         "handle_vertices": {
-        #             "ogit/_xid": "haas1000:connector1:machine1"
-        #         }
-        #     },
-        #     {
-        #         "handle_vertices": {
-        #             "ogit/_xid": "haas1000:connector2:machine2"
-        #         }
-        #     }
-        # ]
-        #
-        # query_results: list = hiro_batch_client.multi_command(commands)
-        #
-        # print(query_results)
-        #
-        # assert isinstance(query_results, list)
+        hiro_batch_client: HiroGraphBatch = HiroGraphBatch(api_handler=self.hiro_api_handler)
+
+        commands: list = [
+            {
+                "handle_vertices": {
+                    "ogit/_xid": "haas1000:connector1:machine1"
+                }
+            },
+            {
+                "handle_vertices": {
+                    "ogit/_xid": "haas1000:connector2:machine2"
+                }
+            }
+        ]
+
+        query_results: list = hiro_batch_client.multi_command(commands)
+
+        print(query_results)
+
+        assert isinstance(query_results, list)
 
     def test_batch_command_callback(self):
-        pass
-        # batch_runner: RunBatch = RunBatch(api_handler=self.hiro_api_handler)
-        #
-        # commands: list = [
-        #     {
-        #         "handle_vertices": {
-        #             "ogit/_xid": "haas1000:connector1:machine1"
-        #         }
-        #     },
-        #     {
-        #         "handle_vertices": {
-        #             "ogit/_xid": "haas1000:connector2:machine2"
-        #         }
-        #     }
-        # ]
-        #
-        # batch_runner._run(commands)
+        batch_runner: RunBatch = RunBatch(api_handler=self.hiro_api_handler)
+
+        commands: list = [
+            {
+                "handle_vertices": {
+                    "ogit/_xid": "haas1000:connector1:machine1"
+                }
+            },
+            {
+                "handle_vertices": {
+                    "ogit/_xid": "haas1000:connector2:machine2"
+                }
+            }
+        ]
+
+        batch_runner.run(commands)
